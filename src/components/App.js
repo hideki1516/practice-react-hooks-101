6844c7d962e9b028d7718c30d11b9cb76c7c74c2
hideki <hideki.1516.web@gmail.com>
2022-02-22 23:15:56 +0900

    save state with localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm';
@@ -7,12 +7,27 @@ import OperationLogs from './OperationLogs';
 import AppContext from '../contexts/AppContext';
 import reducer from '../reducers';
 
+const APP_KEY = 'appWithRedux';
+
 const App = () => {
-  const initialState = {
+  // getItemでlocalStorageの内容を取り出す ... appState変数にstringを保持
+  const appState = localStorage.getItem(APP_KEY);
+  // 文字列が入っている場合は採用する
+  // そうでない場合はevents,operationLogsに空配列（初期化のstate）
+  // JSON.parse() ... 文字列から元々のstateの形（配列）に戻す
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   };
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    // JSON.stringify() : localStorageに保存するには文字列化させる
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  },[state])
+
+  // stateの変更のたびにuseEffectを実行させる
+  // useEffect : 第1引数にコールバック関数、第2引数に監視する状態を指定
 
   return (    
     <AppContext.Provider value={{ state, dispatch }}>
